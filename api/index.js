1f83cac384fa625c649eecbb931298d57319dfbb
Jitendra <jitendra@pop-os.localdomain>
2023-12-04 08:14:56 +0530

    added database

--- api/index.js
+++ api/index.js
@@ -1,14 +1,39 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-dotenv.config()
-mongoose.connect(process.env.MONGO).then( () => {
-    console.log("connected to database");
-}).catch( (err) =>{
-    console.log("err");
-});
-const app = express();
-app.listen(3000, () => {
-    console.log("serve is running on port:3000");
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+import { STATUS_CODES } from 'http';
+dotenv.config();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
 });
+
+app.use('/api/user', userRoutes);
+app.use("/api/auth", authRoutes)
+
+app.use((err, req, res , next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+return res.status(statusCode).json({
+success:false,
+message,
+ statusCode,
+});
+
+
+})
